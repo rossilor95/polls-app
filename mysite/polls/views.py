74719b1b3b46efb4d8b8ce40f40b1a23c5f80a77
rossilor95 <rossilor95@outlook.it>
2023-06-04 23:20:20 +0200

    refactor: update index view to use template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = { "latest_question_list": latest_question_list }
+    return render(request, "polls/index.html", context)
+    
 
 def detail(request, question_id):
     return HttpResponse(f"You're looking at the response of question {question_id}")
@@ -15,3 +20,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse(f"You're voting on question {question_id}")
+    

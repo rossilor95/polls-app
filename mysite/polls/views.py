c8c7d97afcce6a812db7067b581a31451af5b7ba
rossilor95 <rossilor95@outlook.it>
2023-06-05 23:02:47 +0200

    refactor: add error handling to detail view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
 from .models import Question
@@ -6,12 +6,16 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = { "latest_question_list": latest_question_list }
+    context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
-    
+
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at the response of question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
@@ -20,4 +24,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse(f"You're voting on question {question_id}")
-    
